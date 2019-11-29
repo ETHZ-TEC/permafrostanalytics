d93ecb152e5aea2aeca482916d99e5e3111443b9
Andrea Ziani <andrea.ziani@studio.unibo.it>
2019-11-29 14:42:49 +0100

    report page

--- ideas/matterscout/routes/index.js
+++ ideas/matterscout/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MS' });
 });
 
+router.get('/report', function(req, res, next) {
+  res.render('report', { title: 'MS' });
+});
 module.exports = router;
