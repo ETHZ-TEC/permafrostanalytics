9b7ca21fd5c2b10e546e63256318d3a81bb442af
Simone <simone.barbaro2@studio.unibo.it>
2019-11-29 12:03:20 +0100

    added image getter

--- ideas/matterscout/anomaly_detection.py
+++ ideas/matterscout/anomaly_detection.py
@@ -1,10 +1,55 @@
 import stuett
-from stuett.global_config import get_setting, setting_exists
+import torch
+import numpy as np
+import scipy
 import argparse
-from pathlib import Path
-from datetime import datetime
-import plotly.graph_objects as go
+import datetime as dt
+import os
 import pandas as pd
+import xarray as xr
+
+from datasets import SeismicDataset, DatasetFreezer, DatasetMerger
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch import Tensor
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+
+
+from plotly import tools
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import plotly.graph_objs as go
+
+import stuett
+from stuett.global_config import get_setting, setting_exists, set_setting
+
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import Dataset
+from ignite.metrics import Accuracy
+
+from pathlib import Path
+
+from PIL import Image
+
+import numpy as np
+import json
+import pandas as pd
+import os
+from skimage import io as imio
+import io, codecs
+
+from models import SimpleCNN
+
+
 
 account_name = (
     get_setting("azure")["account_name"]
@@ -21,3 +66,44 @@ store = stuett.ABSStore(
     account_key=account_key,
 )
 
+def get_seismic_transform():
+    def to_db(x,min_value=1e-10,reference=1.0):
+        value_db = 10.0 * xr.ufuncs.log10(xr.ufuncs.maximum(min_value, x))
+        value_db -= 10.0 * xr.ufuncs.log10(xr.ufuncs.maximum(min_value, reference))
+        return value_db
+
+    spectrogram = stuett.data.Spectrogram(nfft=512, stride=512, dim="time", sampling_rate=1000)
+
+    transform = transforms.Compose([
+        lambda x: x / x.max(),                          # rescale to -1 to 1
+        spectrogram,                                    # spectrogram
+        lambda x: to_db(x).values.squeeze(),
+        lambda x: Tensor(x)
+        ])
+
+    return transform
+
+
+# Load the data source
+def load_seismic_source():
+    seismic_channels = ["EHE", "EHN", "EHZ"]
+    seismic_node = stuett.data.SeismicSource(
+        store=store,
+        station="MH36",
+        channel=seismic_channels,
+    )
+    return seismic_node, len(seismic_channels)
+
+def load_image_source():
+    image_node = stuett.data.MHDSLRFilenames(
+        store=store,
+        force_write_to_remote=True,
+        as_pandas=False,
+    )
+    return image_node, 3
+
+transform = get_seismic_transform()
+data_node, num_channels = load_seismic_source()
+data = data_node()
+print(type(data))
+print(data)
---
+++ ideas/matterscout/anomaly_visualization.py
@@ -0,0 +1,8 @@
+import stuett
+
+def get_images_from_timestamps(store, start, end):
+    return stuett.data.MHDSLRFilenames(store=store,
+                                       start_time=start,
+                                       end_time=end,
+                                       as_pandas=True)
+
