ebdf0fed49cb8aa2ecba645c193b9d27be70bb71
Andrea Ziani <andrea.ziani@studio.unibo.it>
2019-11-29 11:51:28 +0100

    include ulkit

--- ideas/matterscout/app.js
+++ ideas/matterscout/app.js
@@ -2,12 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +13,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
