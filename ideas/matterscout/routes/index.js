c3a6820b4d980079e26b7f101a67e51937bd0a51
OsD97 <gianluca.lain97@gmail.com>
2019-11-29 15:45:51 +0100

    event details

--- ideas/matterscout/routes/index.js
+++ ideas/matterscout/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var result_data = {
+  "1" : {
+    "name": "Bastard avalanche",
+    "description":"Oh man, fucking run away",
+    "images": [1,2,3,4,5],
+    "sensors": "1.csv",
+    "time":"29-08-2019"
+  },
+  "2" : {
+    "name": "Bastard avalanche",
+    "description":"Oh man, fucking run away",
+    "images": [1,2,3,4,5],
+    "sensors": "1.csv",
+    "time":"29-08-2019"
+  }
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MS' });
+});
+
+
+router.get('/event_detail/:id', function(req, res, next) {
+  var event_id = req.params.id;
+  res.render('event', result_data[event_id]);
 });
 
 router.get('/report', function(req, res, next) {
