ac437b3d5459b7df250191209b7469a309970bee
Andrea Ziani <andrea.ziani@studio.unibo.it>
2019-11-30 12:24:15 +0100

    submit slide

--- ideas/matterscout/anomaly_visualization.py
+++
@@ -1,9 +0,0 @@
-import stuett
-
-
-def get_images_from_timestamps(store, start, end):
-    return stuett.data.MHDSLRFilenames(store=store,
-                                       start_time=start,
-                                       end_time=end,
-                                       as_pandas=True)
-
