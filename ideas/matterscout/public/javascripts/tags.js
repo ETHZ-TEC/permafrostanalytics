1df94d7f19cf1701cf1187bbcef5f208647c5d5f
OsD97 <gianluca.lain97@gmail.com>
2019-11-30 11:08:37 +0100

    new functions

--- ideas/matterscout/public/javascripts/tags.js
+++ ideas/matterscout/public/javascripts/tags.js
@@ -1,14 +1,24 @@
+var array_tags = [];
+var counter = 0;
+
+
 $(document).ready(function () {
     $("#tagbutton").on("click",function (e) {
         e.preventDefault();
         let inserted_tag = $('#tag-form').val();
         let tags = $('#labels').html();
 
-        new_tag = '<span class="uk-label">'+inserted_tag+'</span>';
-
+        new_tag = '<span id="tag-'+counter+'" class="uk-label tag-layout">'+inserted_tag+'</span>';
+        array_tags.push(counter);
+        counter+=1;
         $('#labels').html(tags+new_tag);
         $('#tag-form').val("");
 
+    });
+
+    $(".tag-layout").on("click",function (event) {
+        $(event.target.id).css("display", "none");
+        console.log("clicked");
     })
 
-})
+});
