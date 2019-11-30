1519993a4627d3b58d65600a42817df55ba39772
Simone <simone.barbaro2@studio.unibo.it>
2019-11-30 10:23:11 +0100

    completet data saving

--- ideas/matterscout/anomaly_detection.py
+++ ideas/matterscout/anomaly_detection.py
@@ -59,9 +59,9 @@ def calculate_entropy(v):
 
 # extracts statistical features
 def min_max_estractor(row):
-    return  [np.min(row), np.max(row), np.var(row), np.mean(row**2)]#calculate_entropy(row),
-           #np.percentile(row, 1), np.percentile(row, 5), np.percentile(row, 25),
-            #np.percentile(row, 95), np.percentile(row, 95), np.percentile(row, 99)]
+    return [np.min(row), np.max(row), np.var(row), np.mean(row ** 2)]  # calculate_entropy(row),
+    # np.percentile(row, 1), np.percentile(row, 5), np.percentile(row, 25),
+    # np.percentile(row, 95), np.percentile(row, 95), np.percentile(row, 99)]
 
 
 # computes fourier transform of the signal and extracts features
@@ -82,21 +82,21 @@ def fourier_extractor(x):
             peak_70 = fft_values[i]
         if f_values[i] < 40:
             integral40 += fft_values[i]
-            if fft_values[i] > coeff[int(f_values[i]/2)]:
-                coeff[int(f_values[i]/2)] = fft_values[i]
-    return list(coeff ) + [coeff_0, peak_70, integral40, integral125]
+            if fft_values[i] > coeff[int(f_values[i] / 2)]:
+                coeff[int(f_values[i] / 2)] = fft_values[i]
+    return list(coeff) + [coeff_0, peak_70, integral40, integral125]
 
 
 # extracts features from an hour worth of seismic data from three sensors
 def transform_hour(data):
     data = np.array(data)
-    #print(data)
-    #print(data.shape)
-    features=[]
+    # print(data)
+    # print(data.shape)
+    features = []
     for first_dimension in data:
         for row in first_dimension:
-            #print(row.shape)
-            for extractor in [min_max_estractor]:#, fourier_extractor]:
+            # print(row.shape)
+            for extractor in [min_max_estractor]:  # , fourier_extractor]:
                 for element in extractor(row):
                     features.append(element)
     features = np.array(features)
@@ -115,6 +115,7 @@ def get_seismic_data(date):
         start_time=date,
         end_time=date + timedelta(hours=1),
     )())
+
 
 # Load the data source
 def load_seismic_source(start, end):
@@ -145,10 +146,19 @@ def load_image_source():
     )
     return image_node, 3
 
-"""
-os.remove("data/*")
-os.removedirs("data/*")
-"""
+
+import shutil
+
+folder = 'data'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 rock_temperature_node = stuett.data.CsvSource(rock_temperature_file, store=derived_store)
 rock_temperature = rock_temperature_node().to_dataframe()
@@ -162,15 +172,16 @@ prec_node = stuett.data.CsvSource(prec_file, store=derived_store)
 prec = prec_node().to_dataframe()
 prec = prec.reset_index('name').drop(["unit"], axis=1).pivot(columns='name', values='CSV').drop(["position"], axis=1)
 
-dates, seismic_data = load_seismic_source(start=date(2017, 1, 2), end=date(2017, 1, 3))
+dates, seismic_data = load_seismic_source(start=date(2017, 1, 2), end=date(2017, 1, 5))
 seismic_data = np.array(seismic_data)
 seismic_df = pd.DataFrame(seismic_data)
 seismic_df["date"] = dates
 seismic_df = seismic_df.set_index("date")
 
-#dataset = seismic_df.join(rock_temperature).join(prec)
+# dataset = seismic_df.join(rock_temperature).join(prec)
 dataset = seismic_df.join(prec)
-dataset = pd.DataFrame(SimpleImputer(strategy="constant").fit_transform(dataset), index=dataset.index, columns=dataset.columns)
+dataset = pd.DataFrame(SimpleImputer(strategy="constant").fit_transform(dataset), index=dataset.index,
+                       columns=dataset.columns)
 
 print(dataset.describe())
 dataset.to_csv("seismic_prec_temp.csv")
@@ -190,22 +201,28 @@ anomaly_algorithms = [
     ("Local Outlier Factor", LocalOutlierFactor(
         n_neighbors=35, contamination=outliers_fraction))]
 """
-anomaly_algorithms = [("One-Class SVM", svm.OneClassSVM(nu=outliers_fraction, kernel="rbf", gamma=0.1))]
+anomaly_algorithms = [("Isolation Forest", IsolationForest(behaviour='new',
+                                                           contamination=outliers_fraction,
+                                                           random_state=42))]
 
 for name, algorithm in anomaly_algorithms:
 
     y_pred = algorithm.fit_predict(dataset.values)
-    print(dataset[y_pred < 0].count())
-    print(dataset.count())
+    scores = algorithm.decision_function(dataset[y_pred < 0].values)
+    scores_min = scores.min()
+    scores_max = scores.max()
     for date in dataset[y_pred < 0].index:
 
         os.makedirs("data/{}/images/".format(date), exist_ok=True)
+        score = (algorithm.decision_function(
+            dataset.loc[date].values.reshape((1, len(dataset.columns)))) - scores_min) * 5 / (scores_max - scores_min)
+        with open("data/{}/score.txt".format(date), "w") as f:
+            f.write(str(score[0]))
 
         print("event at {}".format(date))
         # print(dataset.loc[date])
         prec.loc[date].to_csv("data/{}/precipitation_data.csv".format(date))
-        print(get_seismic_data(date)[0])
-        #np.savetxt("data/{}/seismic_data.csv".format(date), get_seismic_data(date)[0], delimiter=",")
+
         pd.DataFrame(get_seismic_data(date)[0]).to_csv("data/{}/seismic_data.csv".format(date))
 
         # print(dataset.describe())
@@ -219,6 +236,3 @@ for name, algorithm in anomaly_algorithms:
             print("data/{}/images/{}.png".format(date, key.split("/")[1]))
             imio.imsave("data/{}/images/{}.png".format(date, key.split("/")[1]), img)
             plt.show()
-            break
-            #time.sleep(0.1)
-        # time.sleep(3)
